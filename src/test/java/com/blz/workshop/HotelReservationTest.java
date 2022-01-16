ead3eeb366d82d4528bc2cabe6ce8ed3e446fa43
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-16 16:19:02 +0530

    Added : Program for the Find Cheapest Hotel for Given the range

--- src/test/java/com/blz/workshop/HotelReservationTest.java
+++ src/test/java/com/blz/workshop/HotelReservationTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class HotelReservationTest {
@@ -23,6 +25,20 @@ public class HotelReservationTest {
         hotelReservation.add(hotel3);
         List<Hotel> result = hotelReservation.getHotelList();
         assertEquals(hotels, result);
+    }
+
+    @Test
+    public void whenGivenDateRangeShouldReturnCheapestHotel() {
+        Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
+        Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
+        Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.add(hotel1);
+        hotelReservation.add(hotel2);
+        hotelReservation.add(hotel3);
+        Map<Integer, Hotel> result = hotelReservation.searchFor("10Sep2020", "11Sep2020");
+        result.forEach((k, v) -> System.out.println(v.getName() + " " + k));
+        assertNotNull(result);
     }
 }
 
